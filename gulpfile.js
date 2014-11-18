fef69cea24632899f479b0777fa8057527f2ebb5
Leonardo Merlin <leonardo.merlin@gmail.com>
2014-11-18 16:20:07 -0200

    Run gulp only changed files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 'use strict';
 
 var gulp = require('gulp');
+var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var jpegRecompress = require('imagemin-jpeg-recompress');
 
+var path = {
+  images: {
+    src: 'assets/img-src/*',
+    dest: 'assets/img/'
+  }
+};
+
 gulp.task('images', function() {
-  return gulp.src('assets/img-src/*')
+  return gulp.src(path.images.src)
+    .pipe(changed(path.images.dest))
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         use: [jpegRecompress({loops: 3})]
     }))
-    .pipe(gulp.dest('assets/img/'));
+    .pipe(gulp.dest(path.images.dest));
 });
