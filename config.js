18872e345d55a333883665782220fef57ecd34b9
Leonardo Merlin <leonardo.merlin@gmail.com>
2015-09-22 22:58:09 -0300

    Wrap by a function

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
-'use strict';
+(function(){
+  'use strict';
 
-module.exports = {
-  pages: ['index.html', 'blog/index.html', 'talks/index.html', 'projects/index.html'],
-  
-  src: 'src',
-  tmp: 'tmp',
-  dest: '_site',
+  module.exports = {
+    pages: ['index.html', 'blog/index.html', 'talks/index.html', 'projects/index.html'],
 
-  styles: {
-    src: 'assets/css/*.scss',
-    tmp: 'tmp/assets/css/*.css',
-    dest: 'assets/css/'
-  },
-  
-  images: {
-    src: 'assets/img-src/*',
-    dest: 'assets/img/'
-  }
-};
+    src: 'src',
+    tmp: 'tmp',
+    dest: '_site',
+
+    styles: {
+      src: 'assets/css/*.scss',
+      tmp: 'tmp/assets/css/*.css',
+      dest: 'assets/css/'
+    },
+
+    images: {
+      src: 'assets/img-src/*',
+      dest: 'assets/img/'
+    }
+  };
+})();
